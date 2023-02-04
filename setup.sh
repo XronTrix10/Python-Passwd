e462443ea7631417604da47ed6d950bd7974ef7a
Xron Trix <sreejannaru34@gmail.com>
2023-02-04 12:49:35 +0530

    installation bash script

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Variables
+SOURCE_DIR="/home/ben/scripts"
+DEST_DIR="/etc/app"
+PYTHON_FILE="/home/ben/fileName.py"
+
+# Copy the whole folder
+cp -r $SOURCE_DIR $DEST_DIR
+
+# Make the destination folder hidden
+chmod -R 700 $DEST_DIR
+
+# Create a permanent alias to run the python file
+echo "alias pythonfile='python3 $PYTHON_FILE'" >> ~/.bashrc
